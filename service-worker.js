63585865368c5d45ad83ece912e4e973e42e6eb6
Ruben Shahi <reubenshahi2015@gmail.com>
2025-10-15 03:35:07 +0545

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,8 @@
-const CACHE_NAME = 'const CACHE_NAME = 'my-private-space-v2';
-const urlsToCache = [
-  './index.html',
-  './manifest.json',
-  './assets/chihiro036.jpeg'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME)
-        .map(key => caches.delete(key)))
-    )
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
-});
-';
+const CACHE_NAME = 'my-private-space-v2';
 const urlsToCache = [
     './index.html',
-'./manifest.json',
-'./assets/chihiro036.jpeg'
+    './manifest.json',
+    './assets/chihiro036.jpeg'
 ];
 
 self.addEventListener('install', event => {
@@ -41,8 +14,10 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(keys =>
-        Promise.all(keys.filter(key => key !== CACHE_NAME)
-        .map(key => caches.delete(key)))
+            Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
         )
     );
 });
