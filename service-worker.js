a87b7ec00cab03b2a70e7a6bb0512184bbe16d86
Ruben Shahi <reubenshahi2015@gmail.com>
2025-10-15 02:22:56 +0545

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'const CACHE_NAME = 'my-private-space-v2';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './assets/chihiro036.jpeg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
+';
+const urlsToCache = [
+    './index.html',
+'./manifest.json',
+'./assets/chihiro036.jpeg'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+        Promise.all(keys.filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key)))
+        )
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(resp => resp || fetch(event.request))
+    );
+});
